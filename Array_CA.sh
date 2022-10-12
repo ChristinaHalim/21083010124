d9e0e86467627fb52a40a554cc90ed9cd6b238f6
ChristinaHalim <113276345+ChristinaHalim@users.noreply.github.com>
2022-10-12 17:09:10 +0700

    Add files via upload

---
+++ Array_CA.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinixServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
